8647c039031e582645615c11221a933f4439f17b
tiennv-comartek <tiennv@comartek.com>
2022-12-30 11:47:54 +0700

    change file

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const URI = process.env.DATABASE_URL;
-
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
