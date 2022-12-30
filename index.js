f08e20f341073b9b12547c4b4c0f247b3268fd21
tiennv-comartek <tiennv@comartek.com>
2022-12-30 11:24:42 +0700

    chang env

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-const URI = process.env.DATABASE_URL;
+const URI =
+  "mongodb+srv://admin:Tien1234@cluster0.dkmi02z.mongodb.net/Mern_App1?retryWrites=true&w=majority";
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
